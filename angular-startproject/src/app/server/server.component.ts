dc20e2022c061489cc2ae0d422bc56459537f781
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-08-07 16:24:14 +0200

    Initialize project

---
+++ angular-startproject/src/app/server/server.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+  .online {
+    color: white;
+  }`]
+})
+
+export class ServerComponent {
+  serverId: number = Math.floor(Math.random() * 10);
+  serverStatus: string = 'Offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+}
