c698103f19609d6d0bda65f5346a5889aaa3b1b4
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-08-22 10:32:19 +0200

    Forms finished in shopping and recipe project

--- shoppingRecipes/src/app/components/recipes/recipe.service.ts
+++ shoppingRecipes/src/app/components/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import { Recipe } from "./recipe.model";
 import { Injectable } from "@angular/core";
 import { Ingredient } from "../../shared/ingredient.module";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -27,17 +29,32 @@ export class RecipeService {
     )
   ];
 
-  constructor(private shoppingService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
   }
 
-  getRecipe(id: number) {
-    return this.recipes.slice()[id];
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToList(ingredients: Ingredient[]) {
-    this.shoppingService.addIngredients(ingredients);
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- shoppingRecipes/src/app/components/shopping-list/shopping-list.component.ts
+++ shoppingRecipes/src/app/components/shopping-list/shopping-list.component.ts
@@ -23,6 +23,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     );
   }
 
+  onEditItem(index: number) {
+    this.shoppingService.startedEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
