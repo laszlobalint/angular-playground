a0c1ec5b0e986b5480b7d223149d70cc595ecd0c
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-09-19 15:02:31 +0200

    Custom setup based project

---
+++ structure/bs-config.js
@@ -0,0 +1,6 @@
+export const server = {
+  baseDir: './dist',
+  middleware: {
+    1: require('connect-history-api-fallback')({ index: '/index.html', verbose: true })
+  }
+};
---
+++ structure/webpack.config.common.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ngw = require('@ngtools/webpack');
+
+module.exports = {
+  entry: './src/app/main.ts',
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+        loader: '@ngtools/webpack'
+      },
+      {
+        test: /\.html$/,
+        loaders: ['html-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['raw-loader']
+      }
+    ],
+    exprContextCritical: false
+  },
+  plugins: [
+    new ngw.AngularCompilerPlugin({
+      tsConfigPath: './tsconfig.aot.json',
+      entryModule: './src/app/app.module#AppModule'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
