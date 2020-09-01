abce38f08060a57e65cf0a22bb2e703c9e411d3d
Balint Laszlo <balint.laszlo@t-systems.com>
2020-09-01 16:56:30 +0200

    Component without databinding.

--- app-tutorial/src/app/recipes/recipe-list/recipe-list.component.ts
+++ app-tutorial/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    {
+      name: 'Test',
+      description: 'Test recipe',
+      imagePath:
+        'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FSeries%2F2020-07-medi-monday-one-pot-eggplant-caponata-pasta%2FKitchn_MediMonday_Eggplant_1',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- app-tutorial/src/app/shopping-list/shopping-list.component.ts
+++ app-tutorial/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: Ingredient[] = [
+    { name: 'Apples', amount: 5 },
+    { name: 'Tomatoes', amount: 10 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
