d28095452460c5a3e414f5e906ca65020ca8ff5f
Balint Laszlo <balint.laszlo@t-systems.com>
2020-11-10 13:47:43 +0100

    Routing practice

--- app-tutorial/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ app-tutorial/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +9,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe?: Recipe;
+
+  constructor(private readonly shoppingListService: ShoppingListService) {}
+
+  onToShoppingList(): void {
+    this.recipe.ingredients.forEach((ingredient) => this.shoppingListService.addIngredient(ingredient));
+  }
 }
--- app-tutorial/src/app/recipes/recipe.service.ts
+++ app-tutorial/src/app/recipes/recipe.service.ts
@@ -5,16 +5,24 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   private recipes: Recipe[] = [
     {
-      name: 'Test',
-      description: 'Test recipe',
-      imagePath:
-        'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FSeries%2F2020-07-medi-monday-one-pot-eggplant-caponata-pasta%2FKitchn_MediMonday_Eggplant_1',
+      name: 'Bolognese Pasta',
+      description:
+        'Bolognese sauce known in Italian as ragù alla bolognese, pronounced, ragù bolognese, or simply ragù) is a meat-based sauce in Italian cuisine, typical of the city of Bologna. ',
+      imagePath: 'https://thecozyapron.com/wp-content/uploads/2019/05/spaghetti-bolognese_thecozyapron_1.jpg',
+      ingredients: [
+        { name: 'Meat', amount: 2 },
+        { name: 'Tomatoe', amount: 5 },
+      ],
     },
     {
-      name: 'Fresh',
-      description: 'Fresh recipe',
-      imagePath:
-        'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FSeries%2F2020-07-medi-monday-one-pot-eggplant-caponata-pasta%2FKitchn_MediMonday_Eggplant_1',
+      name: 'Big Burger',
+      description:
+        'A hamburger (also burger for short) is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.',
+      imagePath: 'https://kep.cdn.indexvas.hu/1/0/3032/30329/303292/30329287_f0a7269532d4fe0e03f1e2dea847be7c_wm.jpg',
+      ingredients: [
+        { name: 'Meat', amount: 1 },
+        { name: 'Onion', amount: 2 },
+      ],
     },
   ];
 
